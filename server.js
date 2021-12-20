a6e9e061ffcbc12ecb7a9be9c8c665bffdde243f
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-12-20 18:22:28 -0500

    add FS and path, Create POST request, and functions to create a new animal/validate the information

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+// need fs to be able to write files into the JSON file 
+const fs = require('fs');
+// path is used to help with files and directory paths
+const path = require('path');
 const express = require('express');
 // When Heroku runs our app, it sets an environment variable called process.env.PORT. We're going to tell our app to use that port, if it has been set, and if not, default to port 80.
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+// The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object. 
+// The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+// takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object.
+app.use(express.json());
+// Both of the above middleware functions need to be set up every time you create a server that's looking to accept POST data.
 const { animals } = require('./data/animal.json');
 
 function filterByQuery(query, animalsArray) {
@@ -46,6 +58,38 @@ function findById (id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    // console.log(body);
+// our funciton's main code will go here
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+        // null and 2, are means of keeping our data formatted.
+        // The null argument means we don't want to edit any of our existing data;
+        // The 2 indicates we want to create white space between our values to make it more readable.
+    );
+// return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -62,6 +106,20 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.sendStatus(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+     // if any data in req.body is incorrect, send 400 error back. 400 error. This indicates to the user that our server doesn't have any problems and we can understand their request, but they incorrectly made the request and we can't allow it to work.
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
