5676d5704060a22688f773fa6c60924cf35f3832
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-12-21 11:46:52 -0500

    create and connect routes to front end pages and create fetch request

--- server.js
+++ server.js
@@ -3,17 +3,29 @@ const fs = require('fs');
 // path is used to help with files and directory paths
 const path = require('path');
 const express = require('express');
-// When Heroku runs our app, it sets an environment variable called process.env.PORT. We're going to tell our app to use that port, if it has been set, and if not, default to port 80.
+// When Heroku runs our app, it sets an environment variable called process.env.PORT. We're going to tell
+// our app to use that port, if it has been set, and if not, default to port 80.
 const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or array data
-// The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object. 
-// The extended: true option set inside the method call informs our server that there may be sub-array data nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
+
+// The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST
+// data and converts it to key/value pairings that can be accessed in the req.body object. 
+
+// The extended: true option set inside the method call informs our server that there may be sub-array data
+// nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 // takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object.
 app.use(express.json());
-// Both of the above middleware functions need to be set up every time you create a server that's looking to accept POST data.
+// Both of the above middleware functions need to be set up every time you create a server that's looking 
+// to accept POST data.
+
+// We added some more middleware to our server and used the express.static() method. The way it works is
+// that we provide a file path to a location in our application (in this case, the public folder) and 
+// instruct the server to make these files static resources. This means that all of our front-end code can
+// now be accessed without having a specific server endpoint created for it!
+app.use(express.static('public'));
 const { animals } = require('./data/animal.json');
 
 function filterByQuery(query, animalsArray) {
@@ -120,6 +132,23 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+// route that connects homepage with server
+// respond with an HTML page to display in the browser. So instead of using res.json(), we're using res.sendFile()
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+// route that connects animals page with server
+app.get('/animals', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+// route that connects zookeepers page with server
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+// wildcard route; and it should always come last
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
